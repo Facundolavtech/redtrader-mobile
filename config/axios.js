8e440e70edb6b7d34314f5de240fc1c75d6a96f6
Facundo Lavagnino/desktop <facuhlavagninodq@gmail.com>
2021-06-11 22:53:38 -0300

    fix notifications

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: "https://redtrader-api.com:9443/api",
-  withCredentials: true,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
